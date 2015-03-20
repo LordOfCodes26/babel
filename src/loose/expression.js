LordOfCodes26/babel

commit 4f7e6f0e4c0b0b563d703cc138f89a7962a05f15

Make the loose parser's parseIdent actually returns an identifier with a name

src/loose/expression.js
@@ -399,9 +399,10 @@ lp.parsePropertyAccessor = function() {
 }
 
 lp.parseIdent = function() {
+  let name = this.tok.type === tt.name ? this.tok.value : this.tok.type.keyword
+  if (!name) return this.dummyIdent()
   let node = this.startNode()
-  node.name = this.tok.type === tt.name ? this.tok.value : this.tok.type.keyword
-  this.next()
+  node.name = name
   return this.finishNode(node, "Identifier")
 }
 

commit ff690e2fcc3123995e4260148dfd164e00690bef

Fix infinite loop introduced by 4f7e6f0

src/loose/expression.js
@@ -402,6 +402,7 @@ lp.parseIdent = function() {
   let name = this.tok.type === tt.name ? this.tok.value : this.tok.type.keyword
   if (!name) return this.dummyIdent()
   let node = this.startNode()
+  this.next()
   node.name = name
   return this.finishNode(node, "Identifier")
 }